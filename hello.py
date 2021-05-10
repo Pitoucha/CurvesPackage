73b540c74bf1011f9eeed8cd6b9c028621aaf751
Emilia <emilia.goeury@outlook.com>
2021-05-10 13:31:40 +0200

    ajout changement Guillaume

--- hello.py
+++ hello.py
@@ -1,8 +1,2 @@
-import sys
 def helloworld():
-	print ("Hello world but in python :)")
-	var = "i was called"
-	return var
-
-if __name__ == '__main__':
-    eval(sys.argv[1] + '()')
+	print "Hello world but in python :)"
