3504390e7b9ef943c9bede521b01b2b5670702f8
Guillaume Schmit <schmit62u@visu6.prod.pct>
2021-05-25 16:21:04 +0200

    Deleted useless code, started color list

--- CurvesPackage.tcl
+++ CurvesPackage.tcl
@@ -40,6 +40,7 @@ namespace eval ::curvespackage:: {
   variable mid
   variable atomsDNA
   set atomsDNA [dict create DA {C1' N6} DT {C1' O4} DC {C1' N4} DG {C1' N1}]
+  variable plotColors {black red green blue magenta orange OliveDrab2 cyan maroon gold2 yellow gray60 SkyBlue2 orchid3 ForestGreen PeachPuff LightSlateBlue}
 }
 
 
@@ -131,53 +132,7 @@ proc ::curvespackage::chargement {} {
     
     #grid for the selection of two bases
     grid [labelframe $w.distG.resSel -text "Select the resnames and resids to be selected" -bd 2] -row 5 -columnspan 6
-    #####################################################################################
-    set COMMENT {
-    #################################
     
-    #dropdown list for using the first base resname
-    grid [ttk::combobox $w.distG.resSel.resNameBase1] -row 1 -column 0
-    #button to add the list of resid values
-    grid [button $w.distG.resSel.getName1 -text "Use this resName"  -command "::curvespackage::selectWithList 0"] -row 1 -column 1
-    #dropdown list for the resid of the first base 
-    grid [ttk::combobox $w.distG.resSel.resIdBase1] -row 1 -column 2
-
-    ###############################
-    #seconde base
-    #dropdown list for the second bases resname
-    grid [ttk::combobox $w.distG.resSel.resNameBase2] -row 2 -column 0
-    #button to add the list of resid values
-    grid [button $w.distG.resSel.getName2 -text "Use this resName"  -command "::curvespackage::selectWithList 1"] -row 2 -column 1
-    #dropdown list for the resid of the second base
-    grid [ttk::combobox $w.distG.resSel.resIdBase2] -row 2 -column 2
-
-    ###############################
-
-    #button for calling the matching of bases
-    grid [button $w.distG.resSel.btnMatch -text "Match this resId to get the facing resId" -command "::curvespackage::matchList"] -row 3 -columnspan 3
-    
-    ###############################
-
-    #third base 
-    #dropdown list for the third bases resname
-    grid [ttk::combobox $w.distG.resSel.resNameMatch1] -row 4 -column 0
-    #button to add the list of resid values
-    grid [button $w.distG.resSel.getName3 -text "Use this resName"  -command "::curvespackage::selectWithList 2"] -row 4 -column 1
-    #dropdown list for the resid of the third base
-    grid [ttk::combobox $w.distG.resSel.resIdMatch1] -row 4 -column 2
-    
-    ###############################
-
-    #fourth base
-    #dropdown list for the fourth bases resname
-    grid [ttk::combobox $w.distG.resSel.resNameMatch2] -row 5 -column 0
-    #button to add the list of resid values
-    grid [button $w.distG.resSel.getName4 -text "Use this resName"  -command "::curvespackage::selectWithList 3"] -row 5 -column 1
-    #dropdown list for the resid of the fourth base
-    grid [ttk::combobox $w.distG.resSel.resIdMatch2] -row 5 -column 2
-
-    }
-    #####################################################################################
     #-command "::curvespackage::selectWithList 0"
     #-command "::curvespackage::selectWithList 2"
     # -command "::curvespackage::selectWithList 1"
@@ -210,25 +165,13 @@ proc ::curvespackage::chargement {} {
     
     #second match
     grid [ttk::combobox $w.distG.resSel.resBase2.resNameMatch2] -row 0 -column 3 -columnspan 2
-    #grid [button $w.distG.resSel.resBase2.getName4 -text "Use this resname"] -row 1 -column 3 -columnspan 2
+    grid [ttk::combobox $w.distG.resSel.resBase2.color -text "Use this resname"] -row 0 -column 5 -columnspan 2
     grid [ttk::combobox $w.distG.resSel.resBase2.resIdMatch2] -row 2 -column 3 -columnspan 2
     
     grid [button $w.distG.resSel.distSel -text "Plot the distance variation between these two bases" -command "::curvespackage::plotBases {dist}"] -row 3
     grid [button $w.distG.resSel.angVal -text "Plot the angle variation between these two bases" -command "::curvespackage::plotBases {angl}"] -row 4
     grid [button $w.distG.resSel.distVal -text "Plot the distance between the two sets of bases " -command "::curvespackage::plotBases {4dist}" -state disabled] -row 5
     grid [button $w.distG.resSel.angleVal -text "Plot the angle between the two sets of bases " -command "::curvespackage::plotBases {4angl}" -state disabled] -row 6
-    #####################################################################################
-    set COMMENT {
-    grid [label $w.distG.resSel.labelComp -text "Select the atom groups to compare"] -row 4 -columnspan 3
-    grid [ttk::combobox $w.distG.resSel.resNameComp1] -row 5 -column 0
-    grid [button $w.distG.resSel.getName3 -text "Use this resName"  -command "::curvespackage::selectWithList 2"] -row 5 -column 1
-    grid [ttk::combobox $w.distG.resSel.resIdComp1] -row 5 -column 2
-    grid [ttk::combobox $w.distG.resSel.resNameComp2] -row 6 -column 0
-    grid [button $w.distG.resSel.getName4 -text "Use this resName"  -command "::curvespackage::selectWithList 3"] -row 6 -column 1
-    grid [ttk::combobox $w.distG.resSel.resIdComp2] -row 6 -column 2
-    grid [button $w.distG.resSel.valSel -text "Plot the angles between the base and the other atoms" -command "::curvespackage::plotAngleVectors"] -row 7 -columnspan 3
-    }
-    #####################################################################################
   
     grid [label $w.distG.frameLab -text "Choose the starting and ending frames to plot, and the step (leave empty for all frames and a step of 1)"] -row 6 -columnspan 6
     grid [label $w.distG.frameSLab -text "First frame :"] -row 7 -column 0
@@ -697,70 +640,6 @@ proc ::curvespackage::plotAngleGroups {} {
                 -marker none -legend "Angle" -plot];
 }
 
-#####################################################################################
-set COMMENT {
-proc ::curvespackage::plotAngleVectors {} {
-  variable w
-  variable frameStart
-  variable frameEnd
-  variable step
-  
-  set startBase [$w.distG.resSel.resIdBase1 get]
-  set endBase [$w.distG.resSel.resIdBase2 get]
-  set startComp [$w.distG.resSel.resIdComp1 get]
-  set endComp [$w.distG.resSel.resIdComp2 get]
-  
-  if { $startBase ne "" && $endBase ne "" && $startComp ne "" && $endComp ne ""} {
-    set startBase [expr int($startBase)]
-    set endBase [expr int($endBase)]
-    set startComp [expr int($startComp)]
-    set endComp [expr int($endComp)]
-    
-    set res1 [atomselect top "resid $startBase"]
-    set res2 [atomselect top "resid $endBase"]
-    set res3 [atomselect top "resid $startComp"]
-    set res4 [atomselect top "resid $endComp"]
-    
-    set lAngl [::curvespackage::computeFrames "angV" $res1 $res2 $res3 $res4]
-    
-    $res1 delete
-    $res2 delete
-    $res3 delete
-    $res4 delete
-    
-    if {$frameStart eq ""} {
-      set frameStart 0
-    } else {
-      set frameStart [expr int($frameStart)]
-    }
-    if {$frameEnd eq ""} {
-      set frameEnd [molinfo top get numframes]
-    } else {
-      set frameEnd [expr int($frameEnd)]
-    }
-    if {$step eq ""} {
-      set step 1
-    } else {
-      set step [expr int($step)]
-    }
-  
-    set xlist {}
-  
-    for { set i $frameStart } { $i < $frameEnd } { set i [expr {$i + $step}] } {
-      lappend xlist $i
-    }
-  
-    set plothandle [multiplot -x $xlist -y $lAngl \
-                  -xlabel "Frame" -ylabel "Angle" -title "Angle between the groups" \
-                  -lines -linewidth 1 -linecolor red \
-                  -marker none -legend "Angle" -plot];
-  } else {
-    puts "Error, some fields are empty"
-  }
-}
-}
-#####################################################################################
-
 proc ::curvespackage::plotBases { type } {
 
   variable w
@@ -1188,65 +1067,11 @@ proc ::curvespackage::computeFrames { type res1 res2 {res3 0} {res4 0} {res5 0} 
       }
       return $lDist
     }
-    "distB" {
-      set lDist {}
-      for { set i $frameStart } { $i < $frameEnd } { set i [expr {$i + $step}] } {
-        $res1 frame $i
-	$res2 frame $i
-	$res3 frame $i
-	$res4 frame $i
-	$res1 update
-	$res2 update
-	$res3 update
-	$res4 update
-	set xyzA1 [split [string range [$res1 get {x y z}] 1 end-1] "\ "]
-	set xyzA2 [split [string range [$res2 get {x y z}] 1 end-1] "\ "]
-	set vect1 [vecsub $xyzA1 $xyzA2]
-	set xyzA1 [split [string range [$res3 get {x y z}] 1 end-1] "\ "]
-	set xyzA2 [split [string range [$res4 get {x y z}] 1 end-1] "\ "]
-	set vect2 [vecsub $xyzA1 $xyzA2]
-	set dist [vecdist $vect1 $vect2]
-	lappend lDist $dist
-      }
-      return $lDist
-    }
     default {
       puts uss
     }
   }
 }
-
-#####################################################################################
-set COMMENT {
-proc ::curvespackage::plotOther {} {
-  puts $::curvespackage::e
-  set res [split $::curvespackage::e "x"]
-  set f " "
-  set i 0
-  foreach s $res {
-    incr i
-    if {[expr {$i != [llength $res]}]} {
-      append f $s
-      append f {$x}
-    }
-  }
-  set xlist {}
-  set ylist {}
-  for {set x -10} {$x <= 10} {set x [expr {$x + 1}]} {
-    if {$x != 0} {
-      lappend xlist $x
-      lappend ylist [expr {[expr $f]*1.0}]
-      puts [expr {[expr $f]*1.0}]
-    }
-  }
-  puts [lindex ylist 15]
-  set plothandle [multiplot -x $xlist -y $ylist \
-                -xlabel "x" -ylabel "$::curvespackage::e" -title "Function $::curvespackage::e" \
-                -lines -linewidth 1 -linecolor red \
-                -marker none -legend "Function $::curvespackage::e" -plot];
-}
-}
-#####################################################################################
 
 proc curvespackage_tk {} {
   ::curvespackage::packageui
--- fenetreTK.tcl
+++ fenetreTK.tcl
@@ -119,3 +119,186 @@ set com {
     #  }
     #}
   }
+  
+  
+  
+  #####################################################################################
+    set COMMENT {
+    #################################
+    
+    #dropdown list for using the first base resname
+    grid [ttk::combobox $w.distG.resSel.resNameBase1] -row 1 -column 0
+    #button to add the list of resid values
+    grid [button $w.distG.resSel.getName1 -text "Use this resName"  -command "::curvespackage::selectWithList 0"] -row 1 -column 1
+    #dropdown list for the resid of the first base 
+    grid [ttk::combobox $w.distG.resSel.resIdBase1] -row 1 -column 2
+
+    ###############################
+    #seconde base
+    #dropdown list for the second bases resname
+    grid [ttk::combobox $w.distG.resSel.resNameBase2] -row 2 -column 0
+    #button to add the list of resid values
+    grid [button $w.distG.resSel.getName2 -text "Use this resName"  -command "::curvespackage::selectWithList 1"] -row 2 -column 1
+    #dropdown list for the resid of the second base
+    grid [ttk::combobox $w.distG.resSel.resIdBase2] -row 2 -column 2
+
+    ###############################
+
+    #button for calling the matching of bases
+    grid [button $w.distG.resSel.btnMatch -text "Match this resId to get the facing resId" -command "::curvespackage::matchList"] -row 3 -columnspan 3
+    
+    ###############################
+
+    #third base 
+    #dropdown list for the third bases resname
+    grid [ttk::combobox $w.distG.resSel.resNameMatch1] -row 4 -column 0
+    #button to add the list of resid values
+    grid [button $w.distG.resSel.getName3 -text "Use this resName"  -command "::curvespackage::selectWithList 2"] -row 4 -column 1
+    #dropdown list for the resid of the third base
+    grid [ttk::combobox $w.distG.resSel.resIdMatch1] -row 4 -column 2
+    
+    ###############################
+
+    #fourth base
+    #dropdown list for the fourth bases resname
+    grid [ttk::combobox $w.distG.resSel.resNameMatch2] -row 5 -column 0
+    #button to add the list of resid values
+    grid [button $w.distG.resSel.getName4 -text "Use this resName"  -command "::curvespackage::selectWithList 3"] -row 5 -column 1
+    #dropdown list for the resid of the fourth base
+    grid [ttk::combobox $w.distG.resSel.resIdMatch2] -row 5 -column 2
+
+    }
+    #####################################################################################
+    
+    #####################################################################################
+    set COMMENT {
+    grid [label $w.distG.resSel.labelComp -text "Select the atom groups to compare"] -row 4 -columnspan 3
+    grid [ttk::combobox $w.distG.resSel.resNameComp1] -row 5 -column 0
+    grid [button $w.distG.resSel.getName3 -text "Use this resName"  -command "::curvespackage::selectWithList 2"] -row 5 -column 1
+    grid [ttk::combobox $w.distG.resSel.resIdComp1] -row 5 -column 2
+    grid [ttk::combobox $w.distG.resSel.resNameComp2] -row 6 -column 0
+    grid [button $w.distG.resSel.getName4 -text "Use this resName"  -command "::curvespackage::selectWithList 3"] -row 6 -column 1
+    grid [ttk::combobox $w.distG.resSel.resIdComp2] -row 6 -column 2
+    grid [button $w.distG.resSel.valSel -text "Plot the angles between the base and the other atoms" -command "::curvespackage::plotAngleVectors"] -row 7 -columnspan 3
+    }
+    #####################################################################################
+    #####################################################################################
+    set COMMENT {
+    "distB" {
+      set lDist {}
+      for { set i $frameStart } { $i < $frameEnd } { set i [expr {$i + $step}] } {
+        $res1 frame $i
+	$res2 frame $i
+	$res3 frame $i
+	$res4 frame $i
+	$res1 update
+	$res2 update
+	$res3 update
+	$res4 update
+	set xyzA1 [split [string range [$res1 get {x y z}] 1 end-1] "\ "]
+	set xyzA2 [split [string range [$res2 get {x y z}] 1 end-1] "\ "]
+	set vect1 [vecsub $xyzA1 $xyzA2]
+	set xyzA1 [split [string range [$res3 get {x y z}] 1 end-1] "\ "]
+	set xyzA2 [split [string range [$res4 get {x y z}] 1 end-1] "\ "]
+	set vect2 [vecsub $xyzA1 $xyzA2]
+	set dist [vecdist $vect1 $vect2]
+	lappend lDist $dist
+      }
+      return $lDist
+    }
+  }
+  #####################################################################################
+  #####################################################################################
+set COMMENT {
+proc ::curvespackage::plotOther {} {
+  puts $::curvespackage::e
+  set res [split $::curvespackage::e "x"]
+  set f " "
+  set i 0
+  foreach s $res {
+    incr i
+    if {[expr {$i != [llength $res]}]} {
+      append f $s
+      append f {$x}
+    }
+  }
+  set xlist {}
+  set ylist {}
+  for {set x -10} {$x <= 10} {set x [expr {$x + 1}]} {
+    if {$x != 0} {
+      lappend xlist $x
+      lappend ylist [expr {[expr $f]*1.0}]
+      puts [expr {[expr $f]*1.0}]
+    }
+  }
+  puts [lindex ylist 15]
+  set plothandle [multiplot -x $xlist -y $ylist \
+                -xlabel "x" -ylabel "$::curvespackage::e" -title "Function $::curvespackage::e" \
+                -lines -linewidth 1 -linecolor red \
+                -marker none -legend "Function $::curvespackage::e" -plot];
+}
+}
+#####################################################################################
+#####################################################################################
+set COMMENT {
+proc ::curvespackage::plotAngleVectors {} {
+  variable w
+  variable frameStart
+  variable frameEnd
+  variable step
+  
+  set startBase [$w.distG.resSel.resIdBase1 get]
+  set endBase [$w.distG.resSel.resIdBase2 get]
+  set startComp [$w.distG.resSel.resIdComp1 get]
+  set endComp [$w.distG.resSel.resIdComp2 get]
+  
+  if { $startBase ne "" && $endBase ne "" && $startComp ne "" && $endComp ne ""} {
+    set startBase [expr int($startBase)]
+    set endBase [expr int($endBase)]
+    set startComp [expr int($startComp)]
+    set endComp [expr int($endComp)]
+    
+    set res1 [atomselect top "resid $startBase"]
+    set res2 [atomselect top "resid $endBase"]
+    set res3 [atomselect top "resid $startComp"]
+    set res4 [atomselect top "resid $endComp"]
+    
+    set lAngl [::curvespackage::computeFrames "angV" $res1 $res2 $res3 $res4]
+    
+    $res1 delete
+    $res2 delete
+    $res3 delete
+    $res4 delete
+    
+    if {$frameStart eq ""} {
+      set frameStart 0
+    } else {
+      set frameStart [expr int($frameStart)]
+    }
+    if {$frameEnd eq ""} {
+      set frameEnd [molinfo top get numframes]
+    } else {
+      set frameEnd [expr int($frameEnd)]
+    }
+    if {$step eq ""} {
+      set step 1
+    } else {
+      set step [expr int($step)]
+    }
+  
+    set xlist {}
+  
+    for { set i $frameStart } { $i < $frameEnd } { set i [expr {$i + $step}] } {
+      lappend xlist $i
+    }
+  
+    set plothandle [multiplot -x $xlist -y $lAngl \
+                  -xlabel "Frame" -ylabel "Angle" -title "Angle between the groups" \
+                  -lines -linewidth 1 -linecolor red \
+                  -marker none -legend "Angle" -plot];
+  } else {
+    puts "Error, some fields are empty"
+  }
+}
+}
+#####################################################################################
