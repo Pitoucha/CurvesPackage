80f361995de3bddc3e23756157cfedcf8d54366a
Emilia <emilia.goeury@outlook.com>
2021-06-14 17:45:41 +0200

    name changed

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,14 +1,14 @@
 ##
-## CurvesPackage 0.1
+## NucleicAnalysor 0.1
 ##
 ## A VMD plugin used to plot data for double helix and G-Quadruplex DNA
 ##
 ## Authors: Emilia Goeury-Mayo ; Guillaume Schmit
 ##
-## Id: CurvesPackage.tcl, v0.1 2021/05/26 17:19
+## Id: NucleicAnalysor.tcl, v0.1 2021/05/26 17:19
 ##
 
-package ifneeded CurvesPackage 0.1 [list source [file join $dir CurvesPackage.tcl]]
+package ifneeded NucleicAnalysor 0.1 [list source [file join $dir NucleicAnalysor.tcl]]
 
 global env 
-set env(CURVESPACKAGE_PATH) $dir
+set env(NUCLEICANALYSOR_PATH) $dir
